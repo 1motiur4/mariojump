0a3563fe9bcecd0fbf47a6336fa7d4c9fd0ecf37
1motiur4 <92241810+1motiur4@users.noreply.github.com>
2024-05-06 22:41:43 -0400

    x vel added

--- marioworld.js
+++ marioworld.js
@@ -3,6 +3,8 @@ class Game {
     this.canvas = document.getElementById("gameCanvas");
     this.ctx = this.canvas.getContext("2d");
     this.marioY = 200;
+    this.marioX = 50;
+    this.marioXVelocity = 0;
     this.jumping = false;
     this.jumpingVelocity = 0;
   }
@@ -20,6 +22,13 @@ class Game {
           this.jump();
         }
       }
+
+      if (event.keyCode === 39) {
+        this.marioXVelocity += 5;
+      }
+      if (event.keyCode === 37) {
+        this.marioXVelocity -= 5;
+      }
     };
   }
 
@@ -28,15 +37,16 @@ class Game {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     // draw the sky
     this.ctx.fillStyle = "lightblue";
-    this.ctx.fillRect(0, 0, this.canvas.width, 200+32);
+    this.ctx.fillRect(0, 0, this.canvas.width, 200 + 32);
 
-    this.ctx.fillStyle = "#C4A484"
+    this.ctx.fillStyle = "#C4A484";
     this.ctx.fillRect(0, 232, this.canvas.width, this.canvas.height - 232);
   }
 
   drawMario() {
     const marioImage = document.getElementById("mario");
-    this.ctx.drawImage(marioImage, 50, this.marioY, 32, 32);
+    this.marioX += this.marioXVelocity;
+    this.ctx.drawImage(marioImage, this.marioX, this.marioY, 32, 32);
   }
 
   jump() {
